2f77ada420652bb43d37111981bf2c30590b09c8
skuxy <skuki@live.com>
2015-12-29 15:29:38 +0100

    A bit of optimization, not sure if anything changed tbh.

--- d1t1.py
+++ d1t1.py
@@ -3,34 +3,36 @@ import sys
 #returns number depending on input parentheses, where '('
 #increments, and ')' decrements number
 def parse_parentheses(parentheses):
-	result = 0
-	for character in parentheses:
-		if character == '(':
-			result = result + 1
-		elif character == ')':
-			result = result - 1
-	return result
+    result = 0
+    for character in parentheses:
+        if character == '(':
+            result = result + 1
+        elif character == ')':
+            result = result - 1
+    return result
+
 
 #similar function, returns position of parenthese that first threw
 #result below zero
 def find_basement(parentheses):
-	result = 0
-	position = 0
-	for character in parentheses:
-		position = position + 1
-		if character == '(':
-			result = result + 1
-		elif character == ')':
-			if result == 0:
-				break
-			result = result - 1
-	return position
+    result = 0
+    position = 0
+    for character in parentheses:
+        position = position + 1
+        if character == '(':
+            result = result + 1
+        elif character == ')':
+            if result == 0:
+                break
+            result = result - 1
+    return position
+
 
 def main():
-	data = open(sys.argv[1]).read()
-	print parse_parentheses(data)
-	print find_basement(data)
+    data = open(sys.argv[1]).read()
+    print parse_parentheses(data)
+    print find_basement(data)
 
 
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
--- d4t1.py
+++ d4t1.py
@@ -1,24 +1,26 @@
 import md5
 import re
 import sys
-import time
 
 #goal is to find minimal number which, when supplemented to given input,
 #digested gives 5 leading zeros
 
 #checked
-def has_n_leading_zeros(hashed_string,n_of_zeroes=5):
-	return re.match(r'0{' + n_of_zeroes + '}.*',hashed_string)
-	
+def has_n_leading_zeros(hashed_string, n_of_zeroes=5):
+    return re.match(r'0{' + n_of_zeroes + '}.*', hashed_string)
+
+
 def find_lowest_hash(secret_key, n_of_zeroes=5):
-	i = 0
-	while True:
-		if has_n_leading_zeros(md5.new(secret_key+str(i)).hexdigest(), n_of_zeroes):
-			return i
-		i = i + 1
-		
+    i = 0
+    while True:
+        if has_n_leading_zeros(md5.new(secret_key + str(i)).hexdigest(), n_of_zeroes):
+            return i
+        i = i + 1
+
+
 def main():
-	print find_lowest_hash(sys.argv[1], sys.argv[2])
+    print find_lowest_hash(sys.argv[1], sys.argv[2])
+
 
 if __name__ == "__main__":
-	main()
+    main()
