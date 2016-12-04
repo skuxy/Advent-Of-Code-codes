f7f60e18cab6d7b7598f3491e28599ea04847dcb
Borna Skukan <skuki@live.com>
2016-12-04 20:24:42 +0100

    Updated first part, and init of second part

--- 2016/day2.hs
+++ 2016/day2.hs
@@ -1,30 +1,34 @@
---Day two, part one
+--Day two, part one and two
 
 import System.IO
 
-type Keypad = [[Int]]
-keypad = [[1,2,3],[4,5,6],[7,8,9]]
+type Keypad = [[Char]]
+keypad = [['1','2','3'],['4','5','6'],['7','8','9']]
+keypad' = ["--1--","-234-","56789","-ABC-","--D--"]
 
 type Point = (Int,Int)
 point = (1,1)
 
 elemKeypad (y,x) = keypad !! y !! x
+elemKeypad' (y,x) = keypad' !! y !! x
 
 incY (a,b) = (a+1,b)
 incX (a,b) = (a,b+1)
 decY (a,b) = (a-1,b)
 decX (a,b) = (a,b-1)
 
-moveKey 'U' (a,b) = if a `elem` [1,2] then decY (a,b) else (a,b)
-moveKey 'D' (a,b) = if a `elem` [0,1] then incY (a,b) else (a,b)
-moveKey 'R' (a,b) = if b `elem` [0,1] then incX (a,b) else (a,b)
-moveKey 'L' (a,b) = if b `elem` [1,2] then decX (a,b) else (a,b)
+length' keypad index = sum ( map (\x -> if (x !! index) == '-' then 0 else 1) keypad)
 
-parseLine [] point = point
-parseLine (x:xs) point = parseLine xs (moveKey x point)
+moveKey 'U' (a,b) keypad'' = if a `elem` [1..length keypad''-1] then decY (a,b) else (a,b)
+moveKey 'D' (a,b) keypad'' = if a `elem` [0..length keypad''-2] then incY (a,b) else (a,b)
+moveKey 'R' (a,b) keypad'' = if b `elem` [0..length (keypad'' !! a )- 2] then incX (a,b) else (a,b)
+moveKey 'L' (a,b) keypad'' = if b `elem` [1..length (keypad'' !! a )- 1] then decX (a,b) else (a,b)
+
+parseLine [] point _  = point
+parseLine (x:xs) point keypad'' = parseLine xs (moveKey x point keypad'') keypad''
 
 main = do
 	content <- readFile "day2.in" 
 	let linesOfFile = lines content
-	let keys = map elemKeypad (map (\x -> parseLine x (1,1)) linesOfFile)
+	let keys = map elemKeypad (map (\x -> parseLine x (1,1) keypad) linesOfFile)
 	print keys
