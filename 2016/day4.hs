1940e4fb5242d61b02ccc3c8d36d57a479c8e3f3
Borna Skukan <skuki@live.com>
2016-12-05 00:24:57 +0100

    Day 4 task 1 done, very unperfect

---
+++ 2016/day4.hs
@@ -0,0 +1,26 @@
+import Data.List
+import System.IO
+import Data.Ord
+import Data.Monoid
+import Data.List.Split
+
+compareTuples :: (Ord a, Ord b) => (a, b) -> (a, b) -> Ordering
+compareTuples (a,b) (c,d) = 
+	case compare c a  of 
+		EQ -> compare b d
+		ne -> ne
+getCheck line = last $ splitOn "-" line
+getWords line = concat $ init $ splitOn "-" line
+
+frequencyOfLetters letts = map (\x -> (length x, head x)) (group  $sort (filter (/='-') letts))
+
+take2 = map snd
+
+checkCode line = first == second
+	where first = take 5 $ take2 $ sortBy compareTuples (frequencyOfLetters $ getWords line)
+	      second = init $ splitOn "[" (getCheck line) !! 1
+
+main = do
+	content <- readFile "day4.in"
+	let a = filter checkCode (lines content)
+	print $ sum $  map (\x -> read (splitOn "[" x !! 0) :: Int) $ map getCheck a
