3bde93529a010b300f008492ce8f33240da35304
Borna Skukan <skuki@live.com>
2017-12-01 12:06:19 +0100

    * Day one

---
+++ 2017/day1.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import fileinput
+
+RESULTING_SUM_FIRST = 0
+RESULTING_SUM_SECOND = 0
+
+for line in fileinput.input():
+    line_length = len(line) - 1
+    for index in range(line_length):
+        if line[index] == line[(index + 1) % (line_length)]:
+            RESULTING_SUM_FIRST += int(line[index])
+        if line[index] == line[int((index + line_length / 2) % line_length)]:
+            RESULTING_SUM_SECOND += int(line[index])
+
+    # Result for the first part
+    print(RESULTING_SUM_FIRST)
+    # Result for the second part
+    print(RESULTING_SUM_SECOND)
