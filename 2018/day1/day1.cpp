0fd1ee3b43025b5403f7bc523ffde75062376ab5
Borna Skukan <borna.skukan@reversinglabs.com>
2018-12-02 14:27:33 +0100

    * Task 2 complete

--- 2018/day1/day1.cpp
+++ 2018/day1/day1.cpp
@@ -6,7 +6,8 @@
 
 int calculate_frequency(std::ifstream &input_file);  // task 1
 
-int freq_to_reach_twice(std::ifstream &input_file);  // task 2
+std::pair<bool, int> freq_to_reach_twice(
+        std::ifstream &input_file, std::set<int> &reached_frequencies, int last_frequency);  // task 2
 
 int calculate_frequency(std::ifstream &input_file)
 {
@@ -22,24 +23,24 @@ int calculate_frequency(std::ifstream &input_file)
     return current_frequency;
 }
 
-int freq_to_reach_twice(std::ifstream &input_file)
+std::pair<bool, int> freq_to_reach_twice(std::ifstream &input_file, std::set<int> &reached_frequencies,
+                                         int last_frequency)
 {
     // task 2
-    std::set<int> reached_frequencies;
-    reached_frequencies.insert(0); // First one
-
+    int current_frequency{last_frequency};
     std::string line;
 
     while ( std::getline(input_file, line))
     {
         int parsed_frequency = std::stoi(line);
-        auto insert_result = reached_frequencies.insert(parsed_frequency);
+        current_frequency += parsed_frequency;
+        auto insert_result = reached_frequencies.insert(current_frequency);
 
         if (! insert_result.second)
         {
-            return parsed_frequency;
+            return std::make_pair(true, current_frequency);
         }
     }
 
-    return 0; // Some fuckery happened
+    return std::make_pair(false, current_frequency);
 }
--- 2018/day1/main.cpp
+++ 2018/day1/main.cpp
@@ -6,23 +6,48 @@
 
 int main()
 {
+    int first_result{0};
     std::ifstream input_file(INPUT_FILE);
-
-    int result{0};
-
     if (input_file.is_open())
     {
-        // result = calculate_frequency(input_file);
-        result = freq_to_reach_twice(input_file);
+        first_result = calculate_frequency(input_file);
     }
     else
     {
         std::cerr << "No input file, idiot!";
         exit(2);
     }
-
-    std::cout << result << std::endl;
     input_file.close();
+
+
+    std::pair<bool, int> second_result{std::make_pair(false, 0)};
+    std::set<int> reached_frequencies;
+
+    while (!second_result.first)
+    {
+        std::ifstream repeatedly_input_file(INPUT_FILE);
+
+        reached_frequencies.insert(0);
+
+        if (repeatedly_input_file.is_open())
+        {
+            second_result = freq_to_reach_twice(repeatedly_input_file, reached_frequencies, second_result.second);
+
+            if (second_result.first)
+            {
+                break;
+            }
+        }
+        else
+        {
+            std::cerr << "No input file, idiot!";
+            exit(2);
+        }
+        repeatedly_input_file.close();
+    }
+
+    std::cout << first_result << std::endl;
+    std::cout << second_result.second << std::endl;
 
     return 0;
 }
